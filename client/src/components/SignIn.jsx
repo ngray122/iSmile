f92c799c6d7e898539884d3dda6dc542afb04820
Nicole Gray <ngray122@gmail.com>
2022-03-24 12:45:12 -0400

    code cleanup in Signin

--- client/src/components/SignIn.jsx
+++ client/src/components/SignIn.jsx
@@ -1,39 +1,25 @@
-import React, { useState } from "react";
-import axios from "axios";
-import { useHistory } from "react-router-dom";
+import React from "react";
 import Registration from "./Registration";
-import Login from "./Login"
-import Container from '@mui/material/Container';
-import Box from '@mui/material/Box';
-import Grid from '@mui/material/Grid'
-
-
-
-
+import Login from "./Login";
+import Grid from "@mui/material/Grid";
 
 const SignIn = (props) => {
   return (
-
-
-
     <Grid
-    p='20px'
-    height='100%'
-    container
-    spacing={1}
-    sx={{ bgcolor: 'primary.light' }}>
+      p="20px"
+      height="100%"
+      container
+      spacing={1}
+      sx={{ bgcolor: "primary.light" }}
+    >
       <Grid item xs={12} sm={6} md={6}>
-      <Registration></Registration>
+        <Registration></Registration>
       </Grid>
       <Grid item xs={12} sm={6} md={6}>
-      <Login></Login>
+        <Login></Login>
       </Grid>
-
-
-
     </Grid>
-  )
+  );
 };
 
 export default SignIn;
-
