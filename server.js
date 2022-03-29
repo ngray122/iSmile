55e8dd2ddba6563a410683fba2961a53b914db93
Nicole Gray <ngray122@gmail.com>
2022-03-29 13:21:24 -0400

    m

--- server.js
+++ server.js
@@ -3,11 +3,7 @@ const cors = require("cors");
 
 // information stored in env
 require("dotenv").config();
-<<<<<<< HEAD
 // console.log("secret log" + process.env.SECRET_KEY);
-=======
-// console.log("my secret ky ===>", process.env.SECRET_KEY)
->>>>>>> feature/add-edit-form
 const cookieParser = require("cookie-parser");
 
 // app is instance of express
@@ -16,13 +12,8 @@ const app = express();
 const port = 8000;
 
 // parse json
-<<<<<<< HEAD
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-=======
 app.use(express.json({ limit: "25mb" }));
 app.use(express.urlencoded({ extended: true, limit: "25mb" }));
->>>>>>> feature/add-edit-form
 const cookies = require("cookie-parser");
 app.use(cookieParser());
 
