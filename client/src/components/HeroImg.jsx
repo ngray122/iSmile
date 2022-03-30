3908176c44fdb2c6a28fb35b5517224338115044
Nicole Gray <ngray122@gmail.com>
2022-03-30 10:58:43 -0400

    hero div and register placement is good

--- client/src/components/HeroImg.jsx
+++ client/src/components/HeroImg.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export const HeroImg = () => {
   return (
-    <div className="container col s6" id="heroImg">
+    <div className="container" id="heroImg">
       <div className="row">
         <div className="">HeroImg</div>
       </div>
