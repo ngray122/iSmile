6584e4e0aeb02a317816e07f59ce5b365538c77e
Nicole Gray <ngray122@gmail.com>
2022-04-05 10:39:32 -0400

    m

---
+++ client/src/components/PostWall.jsx
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from "react";
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
+import axios from "axios";
+import Paper from "@mui/material/Paper";
+import Box from "@mui/material/Box";
+
+import CardPost from "./CardPost";
+
+const PostWall = () => {
+  let history = useHistory();
+
+  let [registeredUser, setRegisteredUSer] = useState({});
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:8000/api/user/getone", { withCredentials: true })
+      .then((res) => {
+        console.log("RESULT on load, GETONE => ", res);
+        if (res.data) {
+          setRegisteredUSer(res.data);
+        }
+      })
+      .catch((err) => {
+        history.push("/");
+        console.log("ERR WHEN GETTING LOGGED IN USER", err);
+      });
+  }, []);
+
+  return <CardPost elevation={6}></CardPost>;
+};
+
+export default PostWall;
