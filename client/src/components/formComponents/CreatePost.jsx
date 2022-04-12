d45220a61f3d64abd6d8dd7c05989e03fd5be076
Nicole Gray <ngray122@gmail.com>
2022-04-11 21:12:49 -0400

    throws err onSubmit that fields are empty when they are not

--- client/src/components/formComponents/CreatePost.jsx
+++ client/src/components/formComponents/CreatePost.jsx
@@ -4,19 +4,19 @@ import axios from "axios";
 import Typography from "@mui/material/Typography";
 import Paper from "@mui/material/Paper";
 import PostForm from "./PostForm";
-import FormControl from "@mui/material/FormControl";
-import { Link } from "react-router-dom/cjs/react-router-dom.min";
 
 const CreatePost = (props) => {
   let history = useHistory();
   let [formInputError, setFormInputError] = useState({});
 
   let [registeredUser, setRegisteredUSer] = useState({});
-
-  let [name, setName] = useState("");
-  let [text, setText] = useState("");
-  let [url, setUrl] = useState("");
   let [photo, setPhoto] = useState("");
+
+  let [formInfo, setFormInfo] = useState({
+    name: "",
+    text: "",
+    url: "",
+  });
 
   const onChangeFileSelectHandler = (e) => {
     e.preventDefault();
@@ -27,10 +27,20 @@ const CreatePost = (props) => {
     let base64String;
     reader.onloadend = () => {
       base64String = reader.result.replace("data:", "").replace(/^.+,/, "");
+      // setFormInfo(base64String);
       setPhoto(base64String);
-      console.log("base64log -> " + base64String);
+      // console.log("base64log -> " + base64String);
     };
     reader.readAsDataURL(fileInput);
+  };
+
+  const onChangeHandler = (e) => {
+    setFormInfo({
+      ...formInfo,
+      [e.target.name]: e.target.value,
+    });
+    console.log("e.target.value ->", e.target.value);
+    console.log("formInfo -> ", formInfo);
   };
 
   // Creates new post for user
@@ -38,9 +48,7 @@ const CreatePost = (props) => {
     e.preventDefault();
     // console.log("submitHandler working");
     const formData = new FormData();
-    formData.append("name", name);
-    formData.append("text", text);
-    formData.append("url", url);
+    formData.append("formInfo", formInfo);
     formData.append("photo", photo);
     axios
       .post("http://localhost:8000/api/posts/create", formData)
@@ -66,114 +74,14 @@ const CreatePost = (props) => {
       <Typography component="legend" variant="h6">
         What made you smile today, {registeredUser.firstName}?
       </Typography>
-      <form encType="multipart/form-data" onSubmit={submitHandler}>
-        <Paper align="center" variant="outlined" mx="auto" p={1}>
-          <FormControl>
-            {/* Form Starts */}
-            <div className="input-field" sx={{ p: "5px" }}>
-              {/* NAME INPUT */}
-              <input
-                id="name"
-                value={name}
-                onChange={(e) => setName(e.target.value)}
-                type="text"
-                name="name"
-              />
-              <label htmlFor="name">Title</label>
-              <span
-                className="helper-text"
-                data-error="wrong"
-                data-success="right"
-              >
-                {formInputError.name?.message}
-              </span>
-            </div>
 
-            {/*  TEXT INPUT */}
-            <div className="input-field" sx={{ p: "5px" }}>
-              <textarea
-                id="text"
-                value={text}
-                onChange={(e) => setText(e.target.value)}
-                className="materialize-textarea"
-                name="text"
-              />
-              <label htmlFor="text">What would you like to say?</label>
-              <span className="helper-text" data-error="wrong">
-                {formInputError.text?.message}
-              </span>
-            </div>
-
-            {/* URL INPUT */}
-            <div className="input-field" sx={{ p: "5px" }}>
-              <input
-                id="url"
-                value={url}
-                onChange={(e) => setUrl(e.target.value)}
-                type="url"
-                // input="url"
-              />
-              <label htmlFor="url">Add link - optional</label>
-              <span className="helper-text" data-error="wrong">
-                {formInputError.url?.message}
-              </span>
-            </div>
-            {/* IMAGE UPLOAD */}
-            <div className="file-field" sx={{ p: "5px" }}>
-              <div className="btn">
-                <i className="material-icons large prefix">photo_camera</i>
-
-                <input
-                  onChange={onChangeFileSelectHandler}
-                  accept=".png, .jpg, .jpeg"
-                  type="file"
-                  id="photo"
-                  name="photo"
-                  // value={photo}
-                  filename="photo"
-                />
-              </div>
-            </div>
-            <div className="file-path-wrapper">
-              <input className="file-path validate" type="text" />
-            </div>
-            <span className="helper-text" data-error="wrong">
-              {formInputError.photo?.message}
-            </span>
-
-            {/* Submit Button */}
-
-            <div>
-              <button
-                className="btn waves-effect waves-light"
-                type="submit"
-                name="action"
-                id="form-button"
-              >
-                Submit
-                <i className="material-icons right">send</i>
-              </button>
-              <button className="btn waves-effect waves-light" id="form-button">
-                <Link id="form-link" to={"/dashboard"}>
-                  Cancel
-                </Link>
-              </button>
-            </div>
-          </FormControl>
-        </Paper>
-      </form>
-
-      {/* <PostForm
-        sx={{ bgcolor: "primary.light" }}
-        elevation={3}
-        onChange={onchangeFileSelectHandler}
+      <PostForm
+        onChangeHandler={onChangeHandler}
+        onChangeFileSelectHandler={onChangeFileSelectHandler}
         submitHandler={submitHandler}
+        formInfo={formInfo}
         formInputError={formInputError}
-        setName={setName}
-        setText={setText}
-        setUrl={setUrl}
-        filename={setPhoto}
-      ></PostForm> */}
+      ></PostForm>
     </Paper>
   );
 };
