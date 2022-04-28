eb62e2143f607fd63bb49e045aebf3c08ca9f41a
Nicole Gray <ngray122@gmail.com>
2022-04-28 11:40:15 -0400

    cleaned up dashboard

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -1,17 +1,12 @@
-import React, { useEffect, useState, useContext } from "react";
-import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
-import axios from "axios";
-import Paper from "@mui/material/Paper";
+import React, { useContext } from "react";
 import Grid from "@mui/material/Grid";
 import Profile from "./Profile";
 import PostWall from "./PostWall";
 import { UserContext } from "./UserContext";
 import RegisteredNavBar from "./RegisteredNavBar";
-import { Redirect } from "react-router-dom";
 
 const Dashboard = () => {
-  const { registeredUser, setRegisteredUser } = useContext(UserContext);
-  console.log("registeredUser inside Dashboard component->", registeredUser);
+  const { registeredUser } = useContext(UserContext);
 
   return (
     <>
