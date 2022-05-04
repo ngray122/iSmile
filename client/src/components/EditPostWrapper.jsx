65d915e02ec8ebd8448dc2b6ffae7b1ea6051dbe
Nicole Gray <ngray122@gmail.com>
2022-05-03 23:01:34 -0400

    editpost wrapper successful

--- client/src/components/EditPostWrapper.jsx
+++ client/src/components/EditPostWrapper.jsx
@@ -3,23 +3,25 @@ import EditPost from "./formComponents/EditPost";
 import Profile from "./Profile";
 import RegisteredNavBar from "./RegisteredNavBar";
 import Grid from "@mui/material/Grid";
+import { useTheme } from "@mui/material/styles";
+import { useMediaQuery } from "@mui/material";
 
 const EditPostWrapper = () => {
+  const theme = useTheme();
+  const matches = useMediaQuery(theme.breakpoints.down("md"));
   return (
     <>
       <RegisteredNavBar />
       <Grid container spacing={3} m={2}>
-        <Grid item xs={12} sm={12} md={5}>
-          <Profile />
-        </Grid>
-        <Grid
-          item
-          xs={12}
-          sm={12}
-          md={7}
-          display="flex"
-          justifyContent="center"
-        >
+        {!matches ? (
+          <Grid item md={4}>
+            <Profile />
+          </Grid>
+        ) : (
+          ""
+        )}
+
+        <Grid item md={8} display="flex" justifyContent="center">
           <EditPost active={true} />
         </Grid>
       </Grid>
