e64f7173dd7cdd7c6071ed98b8cdab08a49a919a
Nicole Gray <ngray122@gmail.com>
2022-05-03 22:32:15 -0400

    createpost wrapper successful

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -37,7 +37,7 @@ function App() {
       values: {
         xs: 0,
         sm: 600,
-        md: 900,
+        md: 850,
         lg: 1200,
         xl: 1536,
       },
--- client/src/components/CreatePostWrapper.jsx
+++ client/src/components/CreatePostWrapper.jsx
@@ -2,26 +2,27 @@ import React from "react";
 import RegisteredNavBar from "./RegisteredNavBar";
 import Profile from "./Profile";
 import Grid from "@mui/material/Grid";
+import { useTheme } from "@mui/material/styles";
 
 import CreatePost from "./formComponents/CreatePost";
-import { Hidden } from "@mui/material";
+import { useMediaQuery } from "@mui/material";
 
 const CreatePostWrapper = () => {
+  const theme = useTheme();
+  const matches = useMediaQuery(theme.breakpoints.down("md"));
   return (
     <>
       <RegisteredNavBar />
       <Grid container spacing={3} m={2}>
-        <Grid item sm={12} md={5}>
-          <Profile />
-        </Grid>
-        <Grid
-          item
-          xs={12}
-          sm={12}
-          md={7}
-          display="flex"
-          justifyContent="center"
-        >
+        {!matches ? (
+          <Grid item md={4}>
+            <Profile />
+          </Grid>
+        ) : (
+          ""
+        )}
+
+        <Grid item md={8} display="flex" justifyContent="center">
           <CreatePost />
         </Grid>
       </Grid>
