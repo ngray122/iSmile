b83c52fb8847e36229c6eb1fac46a2e744ab49f9
Nicole Gray <ngray122@gmail.com>
2022-05-04 13:28:21 -0400

    post wall center

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -5,6 +5,7 @@ import PostWall from "./PostWall";
 import RegisteredNavBar from "./RegisteredNavBar";
 import { useMediaQuery } from "@mui/material";
 import { useTheme } from "@mui/material/styles";
+import Box from "@mui/material/Box";
 
 const Dashboard = () => {
   const theme = useTheme();
@@ -14,27 +15,34 @@ const Dashboard = () => {
       {console.log("matches ->", matches)}
       <RegisteredNavBar />
       <Grid container spacing={3} m={2}>
-        {/* {!matches ? <Grid item md={4} component={Profile} /> : ""} */}
         {!matches ? (
-          <Grid item md={4}>
-            <Profile />
-          </Grid>
+          <>
+            <Grid item md={4}>
+              <Profile />
+            </Grid>
+            <Grid
+              item
+              md={8}
+              display="flex"
+              direction="column-reverse"
+              alignItems="center"
+            >
+              <PostWall />
+            </Grid>
+          </>
         ) : (
-          ""
+          <Grid
+            item
+            xs={12}
+            sm={12}
+            md={12}
+            display="flex"
+            direction="column-reverse"
+            alignItems="center"
+          >
+            <PostWall />
+          </Grid>
         )}
-        {/* <Grid item md={4}>
-          <Profile />
-        </Grid> */}
-
-        <Grid
-          item
-          md={8}
-          display="flex"
-          direction="column-reverse"
-          alignItems="center"
-        >
-          <PostWall />
-        </Grid>
       </Grid>
     </>
   );
