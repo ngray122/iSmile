81a60c29e986601feb38041164d8d44b519c3b4c
Nicole Gray <ngray122@gmail.com>
2022-05-05 09:11:23 -0400

    sign in responsive

--- client/src/components/login-reg-components/Login.jsx
+++ client/src/components/login-reg-components/Login.jsx
@@ -6,8 +6,12 @@ import Button from "@mui/material/Button";
 import { HeroImg } from "./HeroImg";
 import { Link } from "react-router-dom/cjs/react-router-dom.min";
 import styles from "../../static/css/style.css";
+import { useMediaQuery } from "@mui/material";
+import { useTheme } from "@mui/material/styles";
 
 const Login = () => {
+  const theme = useTheme();
+  const matches = useMediaQuery(theme.breakpoints.down("md"));
   let { setRegisteredUser, registeredUser } = useContext(AuthContext);
   let [email, setEmail] = useState("");
   let [password, setPassword] = useState("");
@@ -33,7 +37,82 @@ const Login = () => {
   };
   return (
     <div className="container">
-      <div className="row container">
+      {!matches ? (
+        <div className="row container">
+          <div className="container col s12">
+            <form
+              className="container"
+              autoComplete="off"
+              onSubmit={login}
+              id="login-form"
+            >
+              <div className="row">
+                <h4 id="signIn-header">Sign In</h4>
+                <div className="input-field col s6">
+                  <i className="material-icons prefix">email</i>
+
+                  <input
+                    id="email"
+                    type="text"
+                    onChange={(e) => setEmail(e.target.value)}
+                    input="email"
+                    className="login-form-input"
+                    value={email}
+                  />
+                  <label htmlFor="email">Email</label>
+                  <span className="helper-text" data-error="wrong">
+                    {formInputError.email?.message}
+                  </span>
+                </div>
+              </div>
+
+              <div className="row">
+                <div className="input-field col s6">
+                  <i className="material-icons prefix">account_circle</i>
+
+                  <input
+                    className="login-form-input"
+                    type="password"
+                    id="password-login-form"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    input="password"
+                  />
+                  <label htmlFor="password">Password</label>
+                  <span className="helper-text" data-error="wrong">
+                    {formInputError.password?.message}
+                  </span>
+                </div>
+              </div>
+              <div>
+                <Button
+                  className="large btn waves-effect waves-light center "
+                  type="submit"
+                  name="action"
+                  id="form-button"
+                >
+                  {" "}
+                  Login
+                  <i className="material-icons right">send</i>
+                </Button>
+                <h4>Don't have an account yet?</h4>
+                <Link
+                  to="/register"
+                  className="large btn waves-effect waves-light center "
+                  name="action"
+                  id="form-button"
+                >
+                  Click here to Register
+                </Link>
+              </div>
+            </form>
+          </div>
+
+          <div className="container col s6">
+            <HeroImg />
+          </div>
+        </div>
+      ) : (
         <div className="container col s12">
           <form
             className="container"
@@ -102,11 +181,7 @@ const Login = () => {
             </div>
           </form>
         </div>
-
-        <div className="container col s12">
-          <HeroImg />
-        </div>
-      </div>
+      )}
     </div>
   );
 };
