9c3bb02de01e66e03e77f3de0d2e52be86b27264
Nicole Gray <ngray122@gmail.com>
2022-05-04 20:07:43 -0400

    registration

--- client/src/components/login-reg-components/Registration.jsx
+++ client/src/components/login-reg-components/Registration.jsx
@@ -5,8 +5,12 @@ import Button from "@mui/material/Button";
 import { HeroImg } from "./HeroImg";
 import { Link } from "react-router-dom/cjs/react-router-dom.min";
 import styles from "../../static/css/style.css";
+import { useMediaQuery } from "@mui/material";
+import { useTheme } from "@mui/material/styles";
 
 const User = (props) => {
+  const theme = useTheme();
+  const matches = useMediaQuery(theme.breakpoints.down("md"));
   let [formInputError, setFormInputError] = useState({});
 
   let [firstName, setFirstName] = useState("");
@@ -38,10 +42,136 @@ const User = (props) => {
 
   return (
     <div className="container">
-      <div className="row container">
+      {!matches ? (
+        <div className="row container">
+          <div className="container col s12">
+            <form
+              className="container"
+              onSubmit={register}
+              id="registration-form"
+            >
+              <div className="row">
+                <h4 id="registration-header">Register</h4>
+
+                <div className="input-field col s6">
+                  <i className="material-icons prefix">account_circle</i>
+                  <input
+                    id="first-name-form"
+                    className="reg-form-input"
+                    value={firstName}
+                    onChange={(e) => setFirstName(e.target.value)}
+                    type="text"
+                  />
+                  <label htmlFor="first-name-form">First Name</label>
+                  <span className="helper-text" data-error="wrong">
+                    {formInputError.firstName?.message}
+                  </span>
+                </div>
+              </div>
+              <div className="row">
+                <div className="input-field col s6">
+                  <i className="material-icons prefix">account_circle</i>
+
+                  <input
+                    className="reg-form-input"
+                    type="text"
+                    id="last-name-form"
+                    value={lastName}
+                    onChange={(e) => setLastName(e.target.value)}
+                  />
+                  <label htmlFor="last-name-form">Last Name</label>
+                  <span className="helper-text" data-error="wrong">
+                    {formInputError.lastName?.message}
+                  </span>
+                </div>
+              </div>
+              <div className="row">
+                <div className="input-field col s6">
+                  <i className="material-icons prefix">email</i>
+
+                  <input
+                    className="reg-form-input"
+                    type="text"
+                    id="email-reg-form"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+                  <label htmlFor="email-reg-form">Email</label>
+                  <span className="helper-text" data-error="wrong">
+                    {formInputError.email?.message}
+                  </span>
+                </div>
+              </div>
+
+              <div className="row">
+                <div className="input-field col s6">
+                  <i className="material-icons prefix">lock</i>
+
+                  <input
+                    className="reg-form-input"
+                    type="password"
+                    id="password-reg-form"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    input="password"
+                  />
+                  <label htmlFor="password-reg-form">Password</label>
+                  <span className="helper-text" data-error="wrong">
+                    {formInputError.password?.message}
+                  </span>
+                </div>
+              </div>
+
+              <div className="row">
+                <div className="input-field col s6">
+                  <i className="material-icons prefix">lock</i>
+
+                  <input
+                    type="password"
+                    className="reg-form-input"
+                    id="confirm-password-reg-form"
+                    value={verifyPassword}
+                    onChange={(e) => setVerifyPassword(e.target.value)}
+                    input="password"
+                  />
+                  <label htmlFor="confirm-password-reg-form">
+                    Confirm Password
+                  </label>
+                  <span className="helper-text" data-error="wrong">
+                    {formInputError.password?.message}
+                  </span>
+                </div>
+              </div>
+
+              <div>
+                <Button
+                  className="btn waves-effect waves-light center"
+                  type="submit"
+                  name="action"
+                  id="form-button"
+                >
+                  Register <i className="material-icons right">send</i>
+                </Button>
+                <h6>Already have an account?</h6>
+                <Link
+                  to="/login"
+                  className="large btn waves-effect waves-light center "
+                  name="action"
+                  id="form-button"
+                >
+                  Login
+                </Link>
+              </div>
+            </form>
+          </div>
+
+          <div className="container col s6">
+            <HeroImg />
+          </div>
+        </div>
+      ) : (
         <div className="container col s12">
           <form
-            // autoComplete="off"
             className="container"
             onSubmit={register}
             id="registration-form"
@@ -160,11 +290,7 @@ const User = (props) => {
             </div>
           </form>
         </div>
-
-        <div className="container col s6">
-          <HeroImg />
-        </div>
-      </div>
+      )}
     </div>
   );
 };
