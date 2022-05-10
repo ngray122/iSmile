eb7ed58aa76797d50ac98c0cfc51272faedaccf1
Nicole Gray <ngray122@gmail.com>
2022-05-10 09:35:34 -0400

    db uri

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const cookieParser = require("cookie-parser");
 
 const app = express();
 
-const port = 8000;
+const port = process.env.PORT;
 
 app.use(express.json({ limit: "25mb" }));
 app.use(express.urlencoded({ extended: true, limit: "25mb" }));
@@ -26,4 +26,12 @@ require("./server/config/config");
 require("./server/routes/user.route")(app);
 require("./server/routes/post.route")(app);
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "./client/build")));
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "./", "client", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => res.send("Please set to production"));
+}
 app.listen(port, () => console.log(`Listening on port ${port}`));
