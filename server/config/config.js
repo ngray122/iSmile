43dd6669df3468179475027dac7d8d0bebc0782e
Nicole Gray <ngray122@gmail.com>
2022-05-10 10:36:27 -0400

    your commit message here

--- server/config/config.js
+++ server/config/config.js
@@ -1,12 +1,15 @@
 const mongoose = require("mongoose");
 
-const db_name = process.env.DB_NAME;
+const db_name = "user_db";
 
 mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(
+    `mongodb+srv://ngray122:root@cluster0.tewol.mongodb.net/${db_name}?retryWrites=true&w=majority`,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
   .then(() => console.log("Established a connection to the database"))
   .catch((err) =>
     console.log("Something went wrong when connecting to the database", err)
